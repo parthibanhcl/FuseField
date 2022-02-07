841a38092375fae0fc744a67f600b9b0babd3bcd
Parthiban Manoharan <parthiban.manoharan_ext@novartis.com>
2022-02-07 17:12:55 +0530

    pdf view changes done

--- controllers/mobile/frmHomeController/frmHomeController.js
+++ controllers/mobile/frmHomeController/frmHomeController.js
@@ -4,12 +4,14 @@ define({
     this.view.mainpage.flxContainer.flxQrCode.onClick=this.onNavigateToQrCodeForm;
     this.view.mainpage.flxContainer.flxManual.onClick=this.onManualClick;
     this.view.CustomAlertWithInput.CacelClick=this.onAlertCancel;
-     this.view.CustomAlertWithInput.SubmitClick=this.onAlertSubmit;
+    this.view.CustomAlertWithInput.SubmitClick=this.onAlertSubmit;
     this.view.CustomAlert.onAlertDoneClick=this.onAlertDoneClick;
+    this.view.CustomAlert.showPDFFile = this.showPDFFile;
     this.view.CustomAlertWithInput.referenceText="";
+    this.view.PdfViewer.closePDFView = this.closePDFView;
   },
   onManualClick:function(){
-    this.view.CustomAlertWithInput.setVisibility(true);
+    this.view.CustomAlertWithInput.setVisibility(true);     
   },
   onNavigateToQrCodeForm:function(){
     var form = new kony.mvc.Navigation("frmQRCode");
@@ -29,5 +31,12 @@ define({
   },
   onAlertDoneClick:function(){
     this.view.CustomAlert.setVisibility(false);
-  }
+  },
+  showPDFFile:function(){
+    this.view.PdfViewer.viewPDF("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf");
+  },
+   closePDFView: function() {
+        this.view.PdfViewer.setVisibility(false);
+        
+        },
 });
---
+++ userwidgets/UIComponent.PdfViewer/modules/require/PdfViewerController.js
@@ -0,0 +1,24 @@
+define(function() {
+
+	return {
+		constructor: function(baseConfig, layoutConfig, pspConfig) {
+        this.view.flxHeaderImg.onTouchStart = this.closePDFView;
+
+		},
+        viewPDF: function(pdfURL) {
+        this.view.browserPDFViewer.htmlString = "";
+        this.view.browserPDFViewer.requestURLConfig = {
+        URL: pdfURL,
+        requestMethod: constants.BROWSER_REQUEST_METHOD_GET
+        };
+        this.view.setVisibility(true);
+        },
+ 
+          
+		//Logic for getters/setters of custom properties
+		initGettersSetters: function() {
+
+		}
+	};
+});
+
